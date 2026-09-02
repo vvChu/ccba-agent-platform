3c95186df50078943dfca48f6f3de057dbf99ef8
vvChu <chuvv@ibst-bim.vn>
2026-09-02 14:58:16 +0700

    style(crawler): format imports in ccba_legal/crawler/__init__.py

--- packages/ccba-legal-intel/src/ccba_legal/crawler/__init__.py
+++ packages/ccba-legal-intel/src/ccba_legal/crawler/__init__.py
@@ -9,6 +9,7 @@ from ccba_legal.cdp import (
     MockChromeCDP,
     _check_is_headless,
 )
+from ccba_legal.registry import resolve_project_root
 from ccba_legal.session import (
     CookieVault,
     TVPLCrawlFailedException,
@@ -20,7 +21,6 @@ from ccba_legal.session import (
     sleep_with_jitter,
     verify_tvpl_vip_status,
 )
-from ccba_legal.registry import resolve_project_root
 from ccba_legal.storage import (
     _check_aws_s3,
     _check_google_drive,
